2913aba800b97a5433dc3e4bb1b43f3654212edd
Jiří Charousek <j.charousek@gmail.com>
2026-05-10 10:47:37 +0200

    18th

--- zebricek_app/urls.py
+++ zebricek_app/urls.py
@@ -7,7 +7,7 @@ app_name = 'zebricek_app'
 urlpatterns = [
     # Cesta pro hlavní stránku žebříčku
     #path('', views.zebricek_detail, name='zebricek_index'),
-    path('', views.zebricek_index, name='zebricek_index')
+    path('', views.zebricek_index, name='zebricek_index'),
     
     # Cesta pro manuální posun
     path('manualni-posun/', views.manualni_posun_hrace, name='manualni_posun'),
