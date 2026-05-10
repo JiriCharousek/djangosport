bf9854c9178cfa5e3f376076c172500f6de60483
Jiří Charousek <j.charousek@gmail.com>
2026-05-10 07:56:47 +0200

    urls kaminka

--- core/urls.py
+++ core/urls.py
@@ -3,22 +3,19 @@ from django.urls import path, include
 from django.views.generic import TemplateView # Toto potřebujeme pro index.html
 from django.views.generic.base import RedirectView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Hlavní stránka
-    #path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('', RedirectView.as_view(url='/tenis/liga/26_kaminka_leto_D/'), name='home'),
-    
-    # Ostatní aplikace
+    # 1. Přesměrování úplného základu (sportadmin.cz) do nového adresáře
+    path('', RedirectView.as_view(url='/kaminka/tenis/liga/26_kaminka_leto_D/'), name='home'),
+
+    # 2. Všechny tenisové věci teď budou pod /kaminka/tenis/...
+    path('kaminka/', include([
     path('tenis/', include('tenis_app.urls')),
-    path('fotbal/', include('fotbal_app.urls')),
-    
-    # Autentizace (přihlašování, reset hesla)
+        # Pokud máš další aplikace, dej je sem do seznamu
     path('accounts/', include('django.contrib.auth.urls')),
     
     path('zebricek/', include('zebricek_app.urls')),
-    
-    
-] 
-
+    ])),
+]
