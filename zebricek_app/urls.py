31547c1b783b18b74c1971540a9d62a860e6933a
Jiří Charousek <j.charousek@gmail.com>
2026-05-10 10:41:47 +0200

    17th

--- zebricek_app/urls.py
+++ zebricek_app/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 
-
+# Namespace aplikace
 app_name = 'zebricek_app' 
 
 urlpatterns = [
-    path('', views.zebricek_detail, name='zebricek_app:zebricek_index'),
+    # Cesta pro hlavní stránku žebříčku
+    #path('', views.zebricek_detail, name='zebricek_index'),
+    path('', views.zebricek_index, name='zebricek_index')
+    
+    # Cesta pro manuální posun
     path('manualni-posun/', views.manualni_posun_hrace, name='manualni_posun'),
 ]
 
