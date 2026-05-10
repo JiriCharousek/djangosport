7d43389e9f84e740d09360e480fe3d9063c41b73
Jiří Charousek <j.charousek@gmail.com>
2026-05-10 07:40:17 +0200

    'urls'

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # Hlavní stránka
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    #path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', RedirectView.as_view(url='/tenis/liga/26_kaminka_leto_D/'), name='home'),
     
     # Ostatní aplikace
     path('tenis/', include('tenis_app.urls')),
@@ -18,4 +19,5 @@ urlpatterns = [
     path('zebricek/', include('zebricek_app.urls')),
     
     
-] # <--- Tady smí být jen jedna tahle závorka!
+] 
+
