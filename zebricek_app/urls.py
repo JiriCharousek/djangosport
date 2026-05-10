67cb04b0e7c21a5ab5d1bb4f70ade493d60ee959
Jiří Charousek <j.charousek@gmail.com>
2026-05-10 09:58:43 +0200

    11th

--- zebricek_app/urls.py
+++ zebricek_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'zebricek_app' 
+
 urlpatterns = [
     path('', views.zebricek_detail, name='zebricek_index'),
     path('manualni-posun/', views.manualni_posun_hrace, name='manualni_posun'),
 ]
+
