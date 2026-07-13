0384abbdd4d5229f40599a89e16398e10f5ebd1f
Jiří Charousek <j.charousek@gmail.com>
2026-07-13 17:21:49 +0200

    /kaminka

--- core/urls.py
+++ core/urls.py
@@ -7,15 +7,8 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # Automatické přesměrování z kořene webu na /kaminka/
-    path('', RedirectView.as_view(url='/kaminka/'), name='home_redirect'),
-    
-    # Hlavní stránka aplikace
-    path('kaminka/', TemplateView.as_view(template_name='index.html'), name='index'),
-    
-    path('kaminka/tenis/', include(('tenis_app.urls', 'tenis_app'))),
-    path('kaminka/accounts/', include('django.contrib.auth.urls')),
-    path('kaminka/zebricek/', include(('zebricek_app.urls', 'zebricek_app'))),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('tenis/', include(('tenis_app.urls', 'tenis_app'))),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('zebricek/', include(('zebricek_app.urls', 'zebricek_app'))),
 ]
-
